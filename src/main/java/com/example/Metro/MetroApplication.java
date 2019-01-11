ebd625f92d46fde1627c8f5a497ae6f05e1cbd86
deshmukhyogeshs <deshmukhyogeshs@gmail.com>
2019-01-11 09:05:47 +0530

    init commit

---
+++ src/main/java/com/example/Metro/MetroApplication.java
@@ -0,0 +1,14 @@
+package com.example.Metro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroApplication.class, args);
+	}
+
+}
+
